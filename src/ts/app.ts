e8f959cb75c764262ce447fe478307149af5539d
Dmitry Pigrov <pigrov@gmail.com>
2022-09-01 21:36:18 +0300

    fix

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -8,7 +8,7 @@ console.log(cart.items);
 
 cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
 cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
-cart.add(new Movie(6666, 'Maze Runner', 5000, new Date('2000-10-10'), 'USA', 'SuperCool', ['comedy', 'horror'], 7200000));
+cart.add(new Movie(6666, 'Мстители', 1000, new Date('2002-12-17'), 'США', 'Avenge Assigment!', ['фантастика', 'боевик', 'фентези', 'приключения'], 137));
 
 
 console.log(cart.items);
--- src/ts/domain/Movie.ts
+++ src/ts/domain/Movie.ts
@@ -1,19 +1,14 @@
 import Buyable from './Buyable';
 
-export default class Movie implements Buyable {
-  readonly single: boolean;
-
+export default class Movie implements Buyable{
   constructor(
     readonly id: number,
     readonly name: string,
     readonly price: number,
-    readonly date: Date,
-    readonly country: string,
-    readonly tagline: string,
+    readonly year: Date,
+    readonly counrty: string,
+    readonly slogan: string,
     readonly genre: string[],
-    readonly duration: number,
-  ) {
-    this.genre = [...genre];
-    this.single = true;
-  }
+    readonly time: number,
+  ) {}
 }
