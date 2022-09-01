0a5a96928811bfae6213bff46c3f89f30957c510
Dmitry Pigrov <pigrov@gmail.com>
2022-09-01 21:32:53 +0300

    movie add

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,15 @@
 import Cart from './service/Cart';
 import Book from './domain/Book';
 import MusicAlbum from './domain/MusicAlbum';
+import Movie from './domain/Movie';
 
 const cart = new Cart();
 console.log(cart.items);
 
 cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
 cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+cart.add(new Movie(6666, 'Maze Runner', 5000, new Date('2000-10-10'), 'USA', 'SuperCool', ['comedy', 'horror'], 7200000));
+
 
 console.log(cart.items);
+
